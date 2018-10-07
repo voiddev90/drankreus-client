f6ee4cf05ad82760acbd111a458d234c31c9d44a
Allon de Veen <allondeveen@gmail.com>
2018-10-07 15:07:39 +0200

    Added user model

---
+++ src/model.ts
@@ -0,0 +1,12 @@
+type User = {
+    Id?: number
+    Email: string
+    Password: string
+    FirstName: string
+    LastName: string
+    Street?: string
+    BuildingNumber?: string
+    PostalCode?: string
+    Area?: string
+    Admin: boolean
+}
