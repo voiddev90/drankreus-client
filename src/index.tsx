9d97347d5153e43f6e6b45862b11054a38a66db9
Allon de Veen <allondeveen@gmail.com>
2018-10-10 23:36:19 +0200

    Imported index.css

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Client from './client'
+import './css/index.css'
 declare let module: any
 
 ReactDOM.render(<Client />, document.getElementById("react-root"))
