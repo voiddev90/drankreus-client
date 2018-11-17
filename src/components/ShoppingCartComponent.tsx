d78ed6d44a0dfbf416c389f497f0a02da80e3155
Allon de Veen <allondeveen@gmail.com>
2018-11-17 04:23:35 +0100

    ShoppingCart shows products and is able to remove products

--- src/components/ShoppingCartComponent.tsx
+++ src/components/ShoppingCartComponent.tsx
@@ -1,25 +1,119 @@
 import * as React from 'react'
 import { ReactCookieProps, withCookies, Cookies } from 'react-cookie'
+import { WithGetState, ProductResponse, Product, Option } from '../model'
+import { ShoppingCartItemComponent } from './ShoppingCartItemComponent'
+import Axios, { AxiosResponse, AxiosError } from 'axios'
 
 const clearShoppingCart: (cookies: Cookies) => void = cookies => {
   cookies.remove('shopping-cart')
 }
 
-const ShoppingCartComponent: React.SFC<ReactCookieProps> = (
-  props: ReactCookieProps
-) => {
-  return (
-    <section className="shopping-cart">
-      <h1>Winkelmand</h1>
-      <button
-        disabled={!props.cookies.get('shopping-cart')}
-        type="button"
-        onClick={() => clearShoppingCart(props.cookies)}
-      >
-        Leegmaken
-      </button>
-    </section>
-  )
+const deleteItemFromShoppingCart: (
+  cookies: Cookies
+) => (product: number) => void = cookies => {
+  return (product: number) => {
+    const shoppingCart: number[] = cookies.get('shopping-cart')
+    const i: number = shoppingCart.indexOf(product)
+    shoppingCart.splice(i, 1)
+    console.log(JSON.stringify(shoppingCart))
+    cookies.set('shopping-cart', shoppingCart)
+  }
+}
+
+const distinct = (value: number, index: number, self: number[]) =>
+  self.indexOf(value) === index
+
+type State = WithGetState<ProductResponse>
+
+class ShoppingCartComponent extends React.Component<ReactCookieProps, State> {
+  constructor(props: ReactCookieProps) {
+    super(props)
+
+    this.state = {
+      type: 'loading'
+    }
+  }
+
+  componentDidMount() {
+    const shoppingCart: number[] = this.props.cookies.get('shopping-cart')
+    if (shoppingCart) {
+      const url: string = shoppingCart
+        .filter(distinct)
+        .map((productId: number) => `products=${productId}`)
+        .join('&')
+      Axios.get(`http://localhost:5000/api/product/?index=0&size=100&${url}`)
+        .then((value: AxiosResponse<ProductResponse>) => {
+          this.setState({
+            type: 'loaded',
+            data: Option(value.data)
+          })
+        })
+        .catch((error: AxiosError) => {
+          console.log(error)
+          this.setState({
+            type: 'error'
+          })
+        })
+    } else {
+      this.setState({
+        type: 'loaded',
+        data: {
+          type: 'some',
+          value: {
+            index: 0,
+            size: 100,
+            items: []
+          }
+        }
+      })
+    }
+  }
+
+  render() {
+    const shoppingCart: number[] = this.props.cookies.get('shopping-cart')
+    return (
+      <section className="shopping-cart">
+        <h1>Winkelmand</h1>
+        <div className="cart-items">
+          {this.state.type == 'loading' ? (
+            <>Loading</>
+          ) : this.state.type == 'error' ? (
+            <>Error</>
+          ) : !shoppingCart ||
+            shoppingCart.length == 0 ||
+            this.state.data.type == 'none' ? (
+            <>Winkelmandje is leeg</>
+          ) : (
+            this.state.data.type == 'some' &&
+            this.state.data.value &&
+            this.state.data.value.items
+              .filter(
+                (product: Product) => shoppingCart.indexOf(product.id) != -1
+              )
+              .map((product: Product) => (
+                <ShoppingCartItemComponent
+                  {...product}
+                  key={product.id}
+                  amount={
+                    shoppingCart.filter(value => value == product.id).length
+                  }
+                  onDel={deleteItemFromShoppingCart(this.props.cookies)}
+                />
+              ))
+          )}
+        </div>
+        <div className="cart-controls">
+          <button
+            disabled={!shoppingCart || shoppingCart.length == 0}
+            type="button"
+            onClick={() => clearShoppingCart(this.props.cookies)}
+          >
+            Leegmaken
+          </button>
+        </div>
+      </section>
+    )
+  }
 }
 
 export default withCookies(ShoppingCartComponent)
