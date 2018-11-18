510f107cfc6ff80c3da626a06a8fc8a78e400f2d
Allon de Veen <allondeveen@gmail.com>
2018-11-18 15:48:35 +0100

    Moved acount css to style.css

--- src/components/AccountComponent.tsx
+++ src/components/AccountComponent.tsx
@@ -11,21 +11,20 @@ export default class BaseComponent extends React.Component<Props, State>{
     render() {
         document.title = 'DrankReus - Account'
         return (
-            <div className="account-container">
+            <section className="account">
                 <div className="user-info">
                     <img></img>
                     <p className="user-name"></p>
                     <p className="user-mail"></p>
                     <p className="user-"></p>
                 </div>
-                <div className="button-container">
+                <div className="account-menu">
                     <button className="go-to-cart">Winkelwagen</button>
                     <button className="go-to-wishlist">Favorieten</button>
                     <button className="go-to-history">Geschiedenis</button>
                     <button className="go-to-points">Punten</button>
-
                 </div>
-            </div>
+            </section>
             
         )
     }
