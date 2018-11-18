38db3c5e5882aa2588827f9a1d42ec77cd087866
Allon de Veen <allondeveen@gmail.com>
2018-11-18 16:01:26 +0100

    Add links to favourites, history and cart. Add account loader and route

--- src/components/AccountComponent.tsx
+++ src/components/AccountComponent.tsx
@@ -1,31 +1,32 @@
 import * as React from 'react'
+import { Link } from 'react-router-dom'
 
 type Props = {}
 type State = {}
 
-export default class BaseComponent extends React.Component<Props, State>{
-    constructor(props: Props){
-        super(props)
-    }
-        
-    render() {
-        document.title = 'DrankReus - Account'
-        return (
-            <section className="account">
-                <div className="user-info">
-                    <img></img>
-                    <p className="user-name"></p>
-                    <p className="user-mail"></p>
-                    <p className="user-"></p>
-                </div>
-                <div className="account-menu">
-                    <button className="go-to-cart">Winkelwagen</button>
-                    <button className="go-to-wishlist">Favorieten</button>
-                    <button className="go-to-history">Geschiedenis</button>
-                    <button className="go-to-points">Punten</button>
-                </div>
-            </section>
-            
-        )
-    }
+export default class BaseComponent extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props)
+  }
+
+  render() {
+    document.title = 'DrankReus - Account'
+    return (
+      <section className='account'>
+        <div className='user-info'>
+          <img />
+          <p className='user-name' />
+          <p className='user-mail' />
+          <p className='user-points' />
+          <div className='last-5 history' />
+          <div className='last-5 favourites' />
+        </div>
+        <div className='account-menu'>
+          <Link to='/cart'>Winkelwagen</Link>
+          <Link to='/account/favourites'>Favorieten</Link>
+          <Link to='/account/history'>Geschiedenis</Link>
+        </div>
+      </section>
+    )
+  }
 }
