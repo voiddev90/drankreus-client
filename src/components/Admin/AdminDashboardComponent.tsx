7caea379433eb2406a45037079964c54cb68e34f
Allon de Veen <allondeveen@gmail.com>
2018-12-22 00:14:51 +0100

    Change import

--- src/components/Admin/AdminDashboardComponent.tsx
+++ src/components/Admin/AdminDashboardComponent.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { MainAdminMenuComponent } from './menu/MainAdminMenuComponent';
+import { MainAdminMenuComponent } from './Menu/MainAdminMenuComponent';
 
 type Props = {}
 
