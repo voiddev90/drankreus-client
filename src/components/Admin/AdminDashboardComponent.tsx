dc37f976280f8d354ace68676117851e40c99c78
Allon de Veen <allondeveen@gmail.com>
2018-12-22 00:01:22 +0100

    Add admin main menu and render it in dashboard

--- src/components/Admin/AdminDashboardComponent.tsx
+++ src/components/Admin/AdminDashboardComponent.tsx
@@ -1,7 +1,16 @@
 import * as React from 'react'
+import { MainAdminMenuComponent } from './menu/MainAdminMenuComponent';
 
 type Props = {}
 
 export const AdminDashboardComponent: React.SFC<Props> = (props: Props) => {
-  return <>Admin Dashboard</>
+  return (
+    <section className='admin dashboard'>
+      <MainAdminMenuComponent />
+      <article className='dashboard-content'>
+        <h1 className='h1 page-title'>Dashboard</h1>
+        <p>Hier komen de grafieken.</p>
+      </article>
+    </section>
+  )
 }
