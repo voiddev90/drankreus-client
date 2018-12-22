64ba3e2ebdbf092183e21026807a0f056652a15e
Allon de Veen <allondeveen@gmail.com>
2018-12-22 11:11:51 +0100

    Add browserrouter and switch to match routes for admin pages, use shortcut exact prop in client router

--- src/client.tsx
+++ src/client.tsx
@@ -1,86 +1,89 @@
-import * as React from "react";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-import BaseComponent from "./components/BaseComponent";
-import HomeComponent from "./components/HomeComponent";
-import { PageLoaderComponent } from "./components/PageLoaderComponent";
-import { ProductDetailComponent } from "./components/Products/ProductDetailComponent";
-import { CheckoutLoaderComponent } from "./components/CheckoutLoaderComponent";
-import { ReactCookieProps, withCookies } from "react-cookie";
-import { addToCart } from "./helpers";
-import { AccountLoaderComponent } from "./components/AccountLoaderComponent";
-import { AdminLoaderComponent } from "./components/Admin/AdminLoaderComponent";
+import * as React from 'react'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+import BaseComponent from './components/BaseComponent'
+import HomeComponent from './components/HomeComponent'
+import { PageLoaderComponent } from './components/PageLoaderComponent'
+import { ProductDetailComponent } from './components/Products/ProductDetailComponent'
+import { CheckoutLoaderComponent } from './components/CheckoutLoaderComponent'
+import { ReactCookieProps, withCookies } from 'react-cookie'
+import { addToCart } from './helpers'
+import { AccountLoaderComponent } from './components/AccountLoaderComponent'
+import { AdminLoaderComponent } from './components/Admin/AdminLoaderComponent'
 
-export type ClientProps = ReactCookieProps;
+export type ClientProps = ReactCookieProps
 
 const Client: React.SFC<ClientProps> = clientProps => {
   return (
     <BrowserRouter>
       <Switch>
         <Route
-          exact={true}
-          path="/"
+          exact
+          path='/'
           render={() => {
             return (
               <BaseComponent>
                 <HomeComponent />
               </BaseComponent>
-            );
+            )
           }}
         />
         <Route
-          path="/product/:slug"
+          path='/product/:slug'
           render={props => {
             return (
               <BaseComponent>
-                <ProductDetailComponent {...props} onAdd={addToCart(clientProps.cookies)} />
+                <ProductDetailComponent
+                  {...props}
+                  onAdd={addToCart(clientProps.cookies)}
+                />
               </BaseComponent>
-            );
+            )
           }}
         />
         <Route
-          path="/checkout/:slug"
+          path='/checkout/:slug'
           render={props => {
             return (
               <BaseComponent>
                 <CheckoutLoaderComponent {...props} />
               </BaseComponent>
-            );
+            )
           }}
         />
         <Route
-          path="/account/:slug"
+          path='/account/:slug'
           render={props => {
             return (
               <BaseComponent>
                 <AccountLoaderComponent {...props} />
               </BaseComponent>
-            );
+            )
           }}
         />
         <Route
-          path="/admin/:slug"
+          path='/admin/:slug'
           render={props => {
             return (
               <BaseComponent>
                 <AdminLoaderComponent {...props} />
               </BaseComponent>
-            );
+            )
           }}
         />
         <Route
-          path="/:slug"
+          path='/:slug'
           render={props => {
             return (
               <BaseComponent>
                 <PageLoaderComponent {...props} />
               </BaseComponent>
-            );
+            )
           }}
         />
       </Switch>
     </BrowserRouter>
-  );
-};
+  )
+}
 
 //export default withCookies(Client)
-export default withCookies((Client) as any)
+export default withCookies(Client as any)
--- src/components/Admin/AdminLoaderComponent.tsx
+++ src/components/Admin/AdminLoaderComponent.tsx
@@ -1,20 +1,39 @@
 import * as React from 'react'
-import { RouteComponentProps, Redirect } from 'react-router'
+import { RouteComponentProps, Redirect, Switch, Route } from 'react-router'
 import { AdminDashboardComponent } from './AdminDashboardComponent'
 import { loggedInUserIsAdmin } from '../../helpers'
+import { BrowserRouter } from 'react-router-dom'
 
 type Props = RouteComponentProps<{ slug: string }>
 
 export const AdminLoaderComponent: React.SFC<Props> = (props: Props) => {
   if (loggedInUserIsAdmin()) {
-    switch (props.match.params.slug) {
-      case 'products':
-        return <>Products</>
-      case 'users':
-        return <>Gebruikers</>
-      default:
-        return <AdminDashboardComponent />
-    }
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path='/admin/products'
+            render={() => <>Productenlijst</>}
+          />
+          <Route
+            exact
+            path='/admin/products/new'
+            render={() => <>Nieuw product</>}
+          />
+          <Route
+            exact
+            path='/admin/users'
+            render={() => <>Gebruikerslijst</>}
+          />
+          <Route
+            exact
+            path='/admin/users/new'
+            render={() => <>Nieuwe gebruiker</>}
+          />
+        </Switch>
+      </BrowserRouter>
+    )
   } else {
     return <Redirect to='/' />
   }
