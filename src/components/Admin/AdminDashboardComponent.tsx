625b541cfa590c2231e6dc753b3af0eef443a0ca
Allon de Veen <allondeveen@gmail.com>
2018-12-22 17:51:38 +0100

    Use grid to create layout

--- src/components/Admin/AdminDashboardComponent.tsx
+++ src/components/Admin/AdminDashboardComponent.tsx
@@ -1,16 +1,23 @@
 import * as React from 'react'
-import { MainAdminMenuComponent } from './Menu/MainAdminMenuComponent';
+import { MainAdminMenuComponent } from './Menu/MainAdminMenuComponent'
+import { Grid } from '@material-ui/core'
 
 type Props = {}
 
 export const AdminDashboardComponent: React.SFC<Props> = (props: Props) => {
   return (
     <section className='admin dashboard'>
-      <MainAdminMenuComponent />
-      <article className='dashboard-content'>
-        <h1 className='h1 page-title'>Dashboard</h1>
-        <p>Hier komen de grafieken.</p>
-      </article>
+      <Grid container spacing={24}>
+        <Grid item xs={2}>
+          <MainAdminMenuComponent />
+        </Grid>
+        <Grid item xs={10}>
+          <article className='dashboard-content'>
+            <h1 className='h1 page-title'>Dashboard</h1>
+            <p>Hier komen de grafieken.</p>
+          </article>
+        </Grid>
+      </Grid>
     </section>
   )
 }
