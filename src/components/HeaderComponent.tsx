7e5574b530fd235ea429f31e240040aca835d123
Allon de Veen <allondeveen@gmail.com>
2018-12-31 16:31:09 +0100

    Change markup to match design structure

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -1,13 +1,24 @@
 import * as React from 'react'
 import { NavLink } from 'react-router-dom'
 import { MainMenuComponent } from './Menu/MainMenuComponent';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { UserMainMenuComponent } from './Menu/UserMainMenuComponent';
+import { MenuComponent } from './Menu/MenuComponent';
+import MenuItemComponent from './Menu/MenuItemComponent';
+import { logOut } from '../helpers';
 
 type Props = {}
-type State = {}
+type State = {
+  showSubMenu: boolean
+}
 
 export default class HeaderComponent extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props)
+    this.state = {
+      showSubMenu: false
+    }
   }
 
   render() {
@@ -15,22 +26,19 @@ export default class HeaderComponent extends React.Component<Props, State> {
       <header className='site-header'>
         <div className='site-header-wrapper'>
           <div className='site-title-wrapper'>
-            <h2 className='site-title'>
-              <NavLink to='/'>DrankReus</NavLink>
-            </h2>
-          </div>
-          <div className='search-wrapper'>
-            <form className='search-form'>
-              <input
-                type='text'
-                name='search'
-                placeholder='Voer zoekterm in en druk op enter..'
-              />
-              <button type='submit'>Zoek</button>
-            </form>
+            <h2 className='site-title'>DrankReus</h2>
           </div>
           <MainMenuComponent />
+          <UserMainMenuComponent toggleSubMenu={() => this.setState({ showSubMenu: !this.state.showSubMenu })} />
         </div>
+        {this.state.showSubMenu && <div className='profile-submenu'>
+          <MenuComponent classes='submenu'>
+            <MenuItemComponent to='/profile'>Mijn profiel</MenuItemComponent>
+            <MenuItemComponent to='/favourites'>Mijn wenslijst</MenuItemComponent>
+            <MenuItemComponent to='/history'>Mijn bestelgeschiedenis</MenuItemComponent>
+            <MenuItemComponent to='/' onClick={() => logOut()}>Uitloggen</MenuItemComponent>
+          </MenuComponent>
+        </div>}
       </header>
     )
   }
--- src/components/Menu/MenuItemComponent.tsx
+++ src/components/Menu/MenuItemComponent.tsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom"
 import * as H from 'history'
 
 type Props = {
-  name: string
   to: H.LocationDescriptor
   navLinkProps?: React.AnchorHTMLAttributes<HTMLAnchorElement>
+  onClick?: () => void
 }
 type State = {}
 
@@ -17,8 +17,8 @@ export default class MenuItemComponent extends React.Component<Props, State> {
   render() {
     return (
       <li className="menu-item">
-        <NavLink {...this.props.navLinkProps} to={this.props.to}>
-          {this.props.name}
+        <NavLink {...this.props.navLinkProps} to={this.props.to} onClick={this.props.onClick}>
+          {this.props.children}
         </NavLink>
       </li>
     )
