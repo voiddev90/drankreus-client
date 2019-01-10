dc195e096529c3f09b0e394217982e7ccdaa0b14
Allon de Veen <allondeveen@gmail.com>
2019-01-10 22:32:18 +0100

    Improve lay out with sidebar

--- src/components/ShoppingCartComponent.tsx
+++ src/components/ShoppingCartComponent.tsx
@@ -11,6 +11,7 @@ import {
 } from '../helpers'
 import ShoppingCartRecap from './ShoppingCart/ShoppingCartRecap'
 import { Link } from 'react-router-dom';
+import { SideBar } from './UI/SideBar';
 
 type State = WithGetState<ProductResponse>
 
@@ -60,58 +61,62 @@ class ShoppingCartComponent extends React.Component<ReactCookieProps, State> {
   render() {
     const shoppingCart: number[] = this.props.cookies.get('shopping-cart')
     return (
-      <section className='shopping-cart'>
-        <h1>Winkelmand</h1>
-        <div className='cart-items'>
-          {this.state.type == 'loading' ? (
-            <>Loading</>
-          ) : this.state.type == 'error' ? (
-            <>Error</>
-          ) : !shoppingCart ||
-            shoppingCart.length == 0 ||
-            this.state.data.type == 'none' ? (
-            <>Winkelmandje is leeg</>
-          ) : (
-            this.state.data.type == 'some' &&
-            this.state.data.value && (
-              <table className='shopping-cart-table'>
-                <tr>
-                  <th className='product' colSpan={5}>Product</th>
-                  <th className='price'>Prijs</th>
-                  <th className='amount' colSpan={2}>Aantal</th>
-                </tr>
-                {this.state.data.value.items
-                  .filter(
-                    (product: Product) => shoppingCart.indexOf(product.id) != -1
-                  )
-                  .map((product: Product) => (
-                    <ShoppingCartItemComponent
-                      {...product}
-                      key={product.id}
-                      amount={
-                        shoppingCart.filter(value => value == product.id).length
-                      }
-                      onDel={deleteItemFromShoppingCart(this.props.cookies)}
-                      onAdd={addToCart(this.props.cookies)}
-                    />
-                  ))}
-              </table>
-            )
-          )}
+      <section className='shopping-cart container-fluid'>
+        <div className='shopping-cart-inner row'>
+          <div className='cart-items col-8'>
+            <h1 className='page-title'>Winkelmand</h1>
+            {this.state.type == 'loading' ? (
+              <>Loading</>
+            ) : this.state.type == 'error' ? (
+              <>Error</>
+            ) : !shoppingCart ||
+              shoppingCart.length == 0 ||
+              this.state.data.type == 'none' ? (
+                    <>Winkelmandje is leeg</>
+                  ) : (
+                    this.state.data.type == 'some' &&
+                    this.state.data.value && (
+                      <table className='shopping-cart-table'>
+                        <tr>
+                          <th className='product' colSpan={5}>Product</th>
+                          <th className='price'>Prijs</th>
+                          <th className='amount' colSpan={2}>Aantal</th>
+                        </tr>
+                        {this.state.data.value.items
+                          .filter(
+                            (product: Product) => shoppingCart.indexOf(product.id) != -1
+                          )
+                          .map((product: Product) => (
+                            <ShoppingCartItemComponent
+                              {...product}
+                              key={product.id}
+                              amount={
+                                shoppingCart.filter(value => value == product.id).length
+                              }
+                              onDel={deleteItemFromShoppingCart(this.props.cookies)}
+                              onAdd={addToCart(this.props.cookies)}
+                            />
+                          ))}
+                      </table>
+                    )
+                  )}
+            <div className='cart-controls'>
+              <p>
+                <button
+                  disabled={!shoppingCart || shoppingCart.length == 0}
+                  type='button'
+                  onClick={() => clearShoppingCart(this.props.cookies)}
+                >
+                  Leegmaken
+                  </button>
+              </p>
+            </div>
+          </div>
+          <SideBar type='blank' size={4} >
+            <ShoppingCartRecap />
+            <div className='order-button'><p><Link to='/checkout/order' className="button">Bestellen</Link></p></div>
+          </SideBar>
         </div>
-        <div className='cart-controls'>
-          <p>
-            <button
-              disabled={!shoppingCart || shoppingCart.length == 0}
-              type='button'
-              onClick={() => clearShoppingCart(this.props.cookies)}
-            >
-              Leegmaken
-            </button>
-          </p>
-        </div>
-        <ShoppingCartRecap />
-        <div className='order-button'><p><Link to='/checkout/order' className="button">Bestellen</Link></p></div>
       </section>
     )
   }
