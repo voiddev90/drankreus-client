40241095798f8c94d9e6882cf460d42b1181fa59
Allon de Veen <allondeveen@gmail.com>
2019-01-10 12:48:09 +0100

    Add adobe fonts, fix grid, finish header, typography, finish buttons

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -2,43 +2,43 @@ import * as React from "react"
 import Axios from "axios";
 import { ProductComponent } from "./Products/ProductComponent";
 import { Product } from "../model";
-import { addToCart, isLoggedIn} from "../helpers";
+import { addToCart, isLoggedIn } from "../helpers";
 import { ReactCookieProps } from "react-cookie";
-type Props = ReactCookieProps 
+type Props = ReactCookieProps
 type State = {
-  data :any,
-  isLoaded :boolean
+  data: any,
+  isLoaded: boolean
 }
 
 export default class HomeComponent extends React.Component<ReactCookieProps, State> {
   onAdd: (products: number[]) => void
   constructor(props: Props) {
     super(props)
-    this.state={
+    this.state = {
       data: null,
       isLoaded: false
     }
   }
-  componentDidMount(){
+  componentDidMount() {
     let date = new Date(Date.now());
     Axios.get(`http://localhost:5000/api/Stats/products/?Month=${date.getMonth() + 1}&Year=${date.getFullYear()}`)
-    .then((value: any) => {
-      let min: number[] = [0,0,0];
-      let mostPopular: number[] = [0,0,0];
-      value.data.forEach((element:any) => {
-        let minVal = Math.min(...min)
-        if(element.amount >= minVal){
-          let arr = min.indexOf(minVal);
-          min[arr] = element.amount
-          mostPopular[arr] = element;
-        }
-      });
-      this.setState({
-        ...this.state,
-        data: mostPopular,
-        isLoaded:true
+      .then((value: any) => {
+        let min: number[] = [0, 0, 0];
+        let mostPopular: number[] = [0, 0, 0];
+        value.data.forEach((element: any) => {
+          let minVal = Math.min(...min)
+          if (element.amount >= minVal) {
+            let arr = min.indexOf(minVal);
+            min[arr] = element.amount
+            mostPopular[arr] = element;
+          }
+        });
+        this.setState({
+          ...this.state,
+          data: mostPopular,
+          isLoaded: true
+        })
       })
-    })
   }
   render() {
     document.title = "Drankreus - Home"
@@ -47,17 +47,17 @@ export default class HomeComponent extends React.Component<ReactCookieProps, Sta
         <h1 className="content-title">Welkom bij DrankReus!</h1>
         <p>Wij hebben een alcoholisten korting voor onze leden!</p>
         {this.state.isLoaded ? <section className='product-overview'>
-                {this.state.data.map((value: any) => {                  
-                  return (  
-                    <ProductComponent
-                      product={value.product}
-                      key={value.id}
-                      onAdd={addToCart(this.props.cookies)}
-                    />
-                  )
-                })}
-                /</section> : <div>aan het laden</div>}
-        
+          {this.state.data.map((value: any) => {
+            return (
+              <ProductComponent
+                product={value.product}
+                key={value.id}
+                onAdd={addToCart(this.props.cookies)}
+              />
+            )
+          })}
+        </section> : <div>aan het laden</div>}
+
       </section>
     )
   }
