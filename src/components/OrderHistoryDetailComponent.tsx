f58bb9aaf59ced0f2870039bdaa775a1b44a97fb
Mark Ernst <markiervc@gmail.com>
2019-01-11 20:56:31 +0100

    corrected mistake

--- src/components/OrderHistoryDetailComponent.tsx
+++ src/components/OrderHistoryDetailComponent.tsx
@@ -13,7 +13,6 @@ export const OrderHistoryDetailComponent: React.SFC<Props> = (props: Props) => {
   return (
     <section>
     <div className="order-container" key={props.order.id}>
-        <p className="order-name">Hieronder is een overzicht te zien van de orders met daarin producten die je in het verleden via onze webshop hebt besteld.</p>
         {props.orderProduct.map((value: OrderProduct) => {
                   return (
                     <OrderHistoryProductComponent
