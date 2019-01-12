c8538188ad0278a63669c11573c2e8da0433c246
Vlad <0956458@hr.nl>
2019-01-12 15:55:19 +0100

    fixed frontpage not loading

--- src/components/HomeComponent.tsx
+++ src/components/HomeComponent.tsx
@@ -24,9 +24,9 @@ class HomeComponent extends React.Component<ReactCookieProps, State> {
     getAuthorizedAxiosInstance()
     .get(`Stats/popular/?Month=${date.getMonth() + 1}&Year=${date.getFullYear()}`)
     .then((value: any) => {
-      if(value < 3) return;
+      if(value.data.value.length < 3) return;
       let min: number[] = [0,0,0];
-      let mostPopular: number[] = [0,0,0];
+      let mostPopular: any = [0,0,0];
       value.data.value.forEach((element:any) => {
         let minVal = Math.min(...min)
         if(element.amount >= minVal){
@@ -35,30 +35,40 @@ class HomeComponent extends React.Component<ReactCookieProps, State> {
           mostPopular[arr] = element.product;
         }
       });
+      mostPopular.forEach((element:any) => {
+        Axios.get(`http://localhost:5000/api/Brand/${element.brandId}`)
+        .then((value: any) => {
+          element.brandEntity = value.data[0]
+        })
+      });
+      console.log(mostPopular[1]);
       this.setState({
         ...this.state,
         data: mostPopular,
         isLoaded:true
-      }, ()=> console.log(this.state.data))
+      })
     })
   }
   render() {
     document.title = "Drankreus - Home"
+    console.log(this.state.isLoaded);
     return (
       <section className="home">
         <h1 className="content-title">Welkom bij DrankReus!</h1>
         <p>Wij hebben een alcoholisten korting voor onze leden!</p>
-        {this.state.isLoaded ? <section className='product-overview'>
-                {this.state.data.map((value: Product) => {                  
+        {(this.state.isLoaded) ? <section className='product-overview'>
+                {this.state.data.map((value: Product) => {
+                  console.log(value.brandEntity)   
                   return (  
                     <ProductComponent
                       product={value}
                       key={value.id}
                       onAdd={addToCart(this.props.cookies)}
+                      
                     />
                   )
                 })}
-                /</section> : <div>aan het laden</div>}
+                </section> : <div>aan het laden</div>}
         
       </section>
     )
